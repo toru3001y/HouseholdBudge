ace567d73188b0a4e4d136ee1aebf4e349cc7da1
Toru Yamazaki <toru3001@gmail.com>
2025-07-14 15:08:51 +0900

    Initial commit from Spring Initializr

---
+++ src/main/java/com/example/household_budget/HouseholdBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.example.household_budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseholdBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseholdBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/household_budget/HouseholdBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.household_budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseholdBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
